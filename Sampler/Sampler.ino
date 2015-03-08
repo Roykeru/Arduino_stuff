6f88e620a147918305b678c8f513d57994a54ce9
Roykeru <m.haines333@gmail.com>
2015-03-08 00:50:35 -0500

    Saved the files

--- Sampler/Sampler.ino
+++ Sampler/Sampler.ino
@@ -63,9 +63,14 @@ int speakerPin = 11; // Can be either 3 or 11, two PWM outputs connected to Time
 volatile uint16_t sample;
 byte lastSample;
 int sample_rate = 8000;
+int defaultSampleRate = 8000;
+int analogVal;
+byte readFlag;
 
 void stopPlayback()
 {
+  
+    OCR2A = sounddata_length + lastSample - sample;
     // Disable playback per-sample interrupt.
     TIMSK1 &= ~_BV(OCIE1A);
 
@@ -104,7 +109,7 @@ ISR(TIMER1_COMPA_vect) {
     ++sample;
 }
 
-void startPlayback(int sample_rate)
+void startPlayback()
 {
     pinMode(speakerPin, OUTPUT);
 
@@ -172,17 +177,72 @@ void startPlayback(int sample_rate)
 
 void setup()
 {
+  
+  // clear ADLAR in ADMUX (0x7C) to right-adjust the result
+  // ADCL will contain lower 8 bits, ADCH upper 2 (in last two bits)
+  ADMUX &= B11011111;
+  // Set REFS1..0 in ADMUX (0x7C) to change reference voltage to the
+  // proper source (01)
+  ADMUX |= B01000000;
+  // Clear MUX3..0 in ADMUX (0x7C) in preparation for setting the analog
+  // input
+  ADMUX &= B11110000;
+  // Set MUX3..0 in ADMUX (0x7C) to read from AD8 (Internal temp)
+  // Do not set above 15! You will overrun other parts of ADMUX. A full
+  // list of possible inputs is available in Table 24-4 of the ATMega328
+  // datasheet
+  ADMUX |= 0;
+  // ADMUX |= B00001000; // Binary equivalent
+  
+  // Set ADEN in ADCSRA (0x7A) to enable the ADC.
+  // Note, this instruction takes 12 ADC clocks to execute
+  ADCSRA |= B10000000;
+  // Set ADATE in ADCSRA (0x7A) to enable auto-triggering.
+  //ADCSRA |= B00100000;
+  // Clear ADTS2..0 in ADCSRB (0x7B) to set trigger mode to free running.
+  // This means that as soon as an ADC has finished, the next will be
+  // immediately started.
+  ADCSRB &= B11111000;
+  // Set the Prescaler to 128 (16000KHz/128 = 125KHz)
+  // Above 200KHz 10-bit results are not reliable.
+  ADCSRA |= B00000111;
+  // Set ADIE in ADCSRA (0x7A) to enable the ADC interrupt.
+  // Without this, the internal interrupt will not trigger.
+  ADCSRA |= B00001000;
+  // Enable global interrupts
+  // AVR macro included in <avr/interrupts.h>, which the Arduino IDE
+  // supplies by default.
+  sei();
+  // Kick off the first ADC
+  readFlag = 0;
+  // Set ADSC in ADCSRA (0x7A) to start the ADC conversion
+  ADCSRA |= B01000000;
+  
     pinMode(ledPin, OUTPUT);
     digitalWrite(ledPin, HIGH);
-    startPlayback(8000);
+    startPlayback();
 }
 
 void loop()
 {
-  startPlayback(8000);
+  startPlayback();
+  if(readFlag == 1){
+    sample_rate = defaultSampleRate + analogVal;
+    readFlag = 0;
+  }
   delay(2946);
   stopPlayback();
-  startPlayback(4000);
-  delay(2946*2);
-  stopPlayback();
+}
+
+ISR(ADC_vect) {
+
+		readFlag = 1;
+		analogVal = ADCL | (ADCH << 8);
+		ADMUX = 0x41;
+		ADCSRA |= B01000000;
+
+	
+	// Not needed because free-running mode is enabled.
+	// Set ADSC in ADCSRA (0x7A) to start another ADC conversion
+	// ADCSRA |= B01000000;
 }
